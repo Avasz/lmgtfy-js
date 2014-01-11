92a220e034eb1e1057bdc522b2a9a8b7c8bf0cc1
Nootan Ghimire <nootan.ghimire@gmail.com>
2014-01-11 02:36:59 -0800

    (May be) Checks if it can lmgtfy the selection
    Checks if there is a valid selection and lmgtfyS if there is. except does not do

--- chrome/button.js
+++ chrome/button.js
@@ -1,9 +1,26 @@
 lmgtfy = {
 
 	1: function () {
-			var str = prompt("Enter your keyword here: ");
-			str = str.replace(/\s/g, "+");
-			alert("http://lmgtfy.com/?q=" +str);
+		
+			if(isEmpty(window.getSelection())){
+			    	var str = prompt("Enter your keyword here: ");
+				str = str.replace(/\s/g, "+");	
+				cctoc("http://lmgtfy.com/?q=" +str);
+			}  else {
+				var str = window.getSelection();
+				str = str.replace(/\s/g, "+");
+				cctoc("http://lmgtfy.com/?q=" +str);
+			}
+			
+			
 	},
+	
+	cctoc: function (text) {
+  		window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+	},
+	isEmpty: function(text){
+		text.trim(); if(text=='') return true;
+		return false;
+	}
 
-}
+}		
